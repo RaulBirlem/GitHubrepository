bcd43912045c4c5bba494ed3176f71ffa015c37c
RaulBirlem <raul.birlem@gmail.com>
2021-11-16 16:02:37 -0300

    1
    16/11

--- 0/script.js
+++ 0/script.js
@@ -1,19 +1,19 @@
-function verificar() {
+function Check() {
     var data = new Date()
-    var ano =data.getFullYear()
+    var year =data.getFullYear()
     var fano = window.document.getElementById('txtano')
     var res = window.document.getElementById('res')
-    if (fano.value.length == 0 || Number(fano.value) > ano) {
-        window.alert('Dados incorretos')
+    if (fano.value.length == 0 || Number(fano.value) > year) {
+        window.alert('Incorrect data!')
     }else{
         var fsex = window.document.getElementsByName('radsex')
-        var idade = ano - Number(fano.value)
-        var gênero = ''
+        var years = year - Number(fano.value)
+        var gender = ''
         if (fsex[0].checked) {
-            gênero = 'Homem'
+            gender = 'Men'
         }else if (fsex[1].checked) {
-            gênero = 'Mulher'
+            gender = 'Woman'
         }
-    res.innerHTML = `  ${gênero} com ${idade} anos !` 
+    res.innerHTML = `  ${gender} with ${years} years !` 
     }
 }
